33554e0fc7afcf7821f003190bc3b97a02a6524f
Lucas <lsarappa@frba.utn.edu.ar>
2023-09-19 11:33:17 -0300

    Agrego verificacion para ver si un numero ingresado es binario o no antes de convertirlo a decimal

--- src/logica/BinarioADecimal.java
+++ src/logica/BinarioADecimal.java
@@ -13,9 +13,14 @@ public class BinarioADecimal {
         System.out.println("Ingrese un numero binario: ");
         double numero = teclado.nextInt();
 
-        int numeroEnDecimal = binarioADecimal (numero);
-        System.out.println("El numero binario: " + numero + " corresponde al numero decimal: " + numeroEnDecimal);
+        if (esBinario(numero)) {
+            int numeroEnDecimal = binarioADecimal(numero);
+            System.out.println("El numero binario: " + numero + " corresponde al numero decimal: " + numeroEnDecimal + ".");
+        } else {
+            System.out.println("Error: El numero " + numero + " ingresado no es binario.");
+        }
     }
+
 
 // METODO PARA PASAR UN NUMERO BINARIO A NUMERO ENTERO.
 
@@ -35,6 +40,20 @@ public class BinarioADecimal {
             numeroEnDecimal += (valorNumerico * potencia); // Acumular el valor calculado en númeroEnDecimal.
         }
         return numeroEnDecimal;
+    }
+
+// METODO PARA COMPROBAR QUE EL NUM INGRESADO ES BINARIO.
+    public static boolean esBinario(double numero) {
+
+        String numCadena = Integer.toString((int) numero);
+
+        for (int i = 0; i < numCadena.length(); i++) {
+            char caracter = numCadena.charAt(i);
+            if (caracter != '0' && caracter != '1') {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
